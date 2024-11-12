747f1f9011c75d986c502ca2adb4542011724e63
SaiKartik24 <76741016+SaiKartik24@users.noreply.github.com>
2024-11-12 19:45:46 +0530

    Update app.js

--- Samaritas-Backend/app.js
+++ Samaritas-Backend/app.js
@@ -4,6 +4,9 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 const cors = require('cors');
+const { SecretClient } = require('@azure/keyvault-secrets');
+const { DefaultAzureCredential } = require('@azure/identity');
+
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -76,7 +79,50 @@ app.use((err, req, res, next) => {
 //   origin: 'https://newamericans-demo-h3a4aqbsh6asemcp.eastus2-01.azurewebsites.net',
 //   credentials: true,
 // }));
+// Azure Key Vault setup
+const credential = new DefaultAzureCredential();
+const client = new SecretClient(`https://na-s.vault.azure.net/`, credential);
 
+app.get('/get-secrets', async (req, res) => {
+  // Check if the 'names' query parameter is provided
+  if (!req.query.names) {
+      return res.status(400).send('Missing query parameter: names. Please provide a comma-separated list of secret names.');
+  }
+
+  const secretNames = req.query.names.split(','); // Expecting names as a comma-separated list
+  const secrets = {};
+
+  for (const name of secretNames) {
+      try {
+          const secret = await client.getSecret(name);
+          secrets[name] = secret.value;
+      } catch (error) {
+          console.error(`Failed to get secret ${name}:`, error.message);
+          secrets[name] = null; // or handle differently if needed
+      }
+  }
+
+  res.status(200).json(secrets);
+});
+
+
+// POST endpoint to set a secret
+app.post('/set-secret', async (req, res) => {
+  const { secretName, secretValue } = req.body;
+
+  if (!secretName || !secretValue) {
+      return res.status(400).send('Please provide both secretName and secretValue.');
+  }
+
+  try {
+      await client.setSecret(secretName, secretValue);
+      res.status(200).send(`Secret ${secretName} set successfully.`);
+  } catch (error) {
+      console.error(`Failed to set secret ${secretName}:`, error.message);
+      res.status(500).send(`Failed to set secret: ${error.message}`);
+  }
+});
+//AZUR-VAULT END
 
 app.use(function(err, req, res, next) {
   console.error('Error details:', {
